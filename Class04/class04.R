8132d0ea5aae036e5cb8866952e420f1916b15c3
Angelika Pangaliman <angelikapangaliman@Angelikas-MacBook-Air.local>
2026-03-13 11:43:09 -0700

    add c4

---
+++ Class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 4 Intro to R"
+#' author: "Angelika"
+#' date: "January 16, 2026"
+#' ---
+
+x <- 1:50
+plot(x)
+plot(x, sin(x),typ="l",col="pink",lwd=3, xlab="Is it lunch time yet?")
